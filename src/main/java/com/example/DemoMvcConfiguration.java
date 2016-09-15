ebf640e82548cf037a36b6ef99d0ab4720377094
Petar Tahchiev <paranoiabla@gmail.com>
2016-09-15 13:01:38 +0300

    Remove rest dispatcher servlet

--- src/main/java/com/example/DemoMvcConfiguration.java
+++ src/main/java/com/example/DemoMvcConfiguration.java
@@ -1,4 +1,4 @@
-package com;
+package com.example;
 
 import com.example.DemoRepositoryRestConfigurerAdapter;
 import com.fasterxml.jackson.databind.ObjectMapper;
--- src/main/java/com/example/DemoRestAutoConfiguration.java
+++ src/main/java/com/example/DemoRestAutoConfiguration.java
@@ -1,17 +1,9 @@
 package com.example;
 
-import com.DemoMvcConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.data.rest.RepositoryRestProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.rest.webmvc.RepositoryRestDispatcherServlet;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import javax.servlet.Servlet;
 
 /**
  * @author Petar Tahchiev
@@ -23,18 +15,18 @@ public class DemoRestAutoConfiguration extends WebMvcConfigurerAdapter {
 
     public static final String REST_DISPATCHER_SERVLET_NAME = "repositoryRestDispatcherServlet";
 
-    @Bean(name = { "defaultRepositoryRestDispatcherServlet", REST_DISPATCHER_SERVLET_NAME })
-    @ConditionalOnMissingBean(name = REST_DISPATCHER_SERVLET_NAME)
-    public ServletRegistrationBean defaultRepositoryRestDispatcherServlet(final RepositoryRestProperties repositoryRestProperties) {
-        AnnotationConfigWebApplicationContext webCtx = new AnnotationConfigWebApplicationContext();
-        webCtx.register(DemoMvcConfiguration.class);
-
-        Servlet restDispatcherServlet = new RepositoryRestDispatcherServlet(webCtx);
-        ServletRegistrationBean restDispatcherServletReg = new ServletRegistrationBean(restDispatcherServlet);
-        restDispatcherServletReg.setName(REST_DISPATCHER_SERVLET_NAME);
-        restDispatcherServletReg.setLoadOnStartup(1);
-        restDispatcherServletReg.addUrlMappings(repositoryRestProperties.getBasePath() + "/*");
-
-        return restDispatcherServletReg;
-    }
+//    @Bean(name = { "defaultRepositoryRestDispatcherServlet", REST_DISPATCHER_SERVLET_NAME })
+    //    @ConditionalOnMissingBean(name = REST_DISPATCHER_SERVLET_NAME)
+    //    public ServletRegistrationBean defaultRepositoryRestDispatcherServlet(final RepositoryRestProperties repositoryRestProperties) {
+    //        AnnotationConfigWebApplicationContext webCtx = new AnnotationConfigWebApplicationContext();
+    //        webCtx.register(DemoMvcConfiguration.class);
+    //
+    //        Servlet restDispatcherServlet = new RepositoryRestDispatcherServlet(webCtx);
+    //        ServletRegistrationBean restDispatcherServletReg = new ServletRegistrationBean(restDispatcherServlet);
+    //        restDispatcherServletReg.setName(REST_DISPATCHER_SERVLET_NAME);
+    //        restDispatcherServletReg.setLoadOnStartup(1);
+    //        restDispatcherServletReg.addUrlMappings(repositoryRestProperties.getBasePath() + "/*");
+    //
+    //        return restDispatcherServletReg;
+    //    }
 }
